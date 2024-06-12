88f155a30ae505b0a7f411c1dc59f68fea682206
8bit <yulya_kalashnikova_2002@mail.ru>
2024-06-12 23:31:01 +0700

    graduate work

---
+++ WpfApp1/SaltGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace WpfApp1
+{
+    internal class SaltGenerator
+    {
+        public static byte[] GenerateSalt(int length)
+        {
+            byte[] salt = new byte[length];
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(salt);
+            }
+
+            return salt;
+        }
+    }
+}
